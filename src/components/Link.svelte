d516dd4dab5e6a644e28f76644fefcdcbfc32081
Henrique Ramos <henriquealberone@outlook.com>
2023-06-10 11:29:39 -0300

    feat: create Link component

---
+++ src/components/Link.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import classNames from 'classnames';
+
+	export let href: string;
+
+	const isActive = window.location.pathname === href;
+	console.log(window.location.pathname, href);
+</script>
+
+<a {href} class={classNames('link', { 'link--active': isActive })}><slot /></a>
+
+<style lang="scss">
+	.link {
+		font-family: 'Karla';
+		&--active {
+			font-weight: bold;
+		}
+	}
+</style>
---
+++ tests/components/Link.test.ts
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/svelte';
+import { faker } from '@faker-js/faker';
+import html from 'svelte-htm';
+
+import Link from '$components/Link.svelte';
+
+describe('<Link />', () => {
+	it('renders with `link--active` CSS class if its href is equal to the current page', () => {
+		const href = `/${faker.word.noun()}`;
+		Object.defineProperty(window, 'location', {
+			value: { pathname: href },
+			writable: true
+		});
+		const link = faker.word.verb();
+		const { getByText, rerender } = render(html`<${Link} href=${href}>${link}</${Link}>`);
+
+		expect(getByText(link)).toHaveClass('link--active');
+	});
+});
