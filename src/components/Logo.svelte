4b575f61853b710c379a4324910bed858d34ff08
Henrique Ramos <henriquealberone@outlook.com>
2023-06-23 15:50:01 -0300

    chore: add height and width prop to Logo

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,11 +1,9 @@
-<svg
-	role="img"
-	width="46"
-	height="64"
-	viewBox="0 0 46 64"
-	fill="none"
-	xmlns="http://www.w3.org/2000/svg"
->
+<script lang="ts">
+	export let width = 64;
+	export let height = 64;
+</script>
+
+<svg role="img" {width} {height} viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
 	<path
 		d="M0.369141 18.156H18.4738V0H27.5261V18.156H45.6308V27.234H27.5261V63.5461H18.4738V27.234H0.369141V18.156Z"
 		fill="black"
