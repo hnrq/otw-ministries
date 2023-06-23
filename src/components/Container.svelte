62ba5e321be0c63bbaa8b09e1f7683bbcb7fca7a
Henrique Ramos <henriquealberone@outlook.com>
2023-06-23 16:47:39 -0300

    chore: create Container component

---
+++ src/components/Container.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' = 'lg';
+</script>
+
+<div class={`container container--${size}`}>
+	<slot />
+</div>
+
+<style lang="scss">
+	@use '$theme/utils';
+
+	.container {
+		position: relative;
+		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		padding: 0;
+		box-sizing: border-box;
+		@include utils.breakpoint-down('md') {
+			margin-left: utils.spacing(2);
+			margin-right: utils.spacing(2);
+		}
+
+		@each $breakpoint, $value in utils.$breakpoints {
+			&--#{$breakpoint} {
+				max-width: $value;
+			}
+		}
+	}
+</style>
---
+++ tests/components/Container.test.ts
@@ -0,0 +1,14 @@
+import Container from '$components/Container.svelte';
+import { faker } from '@faker-js/faker';
+import { render } from '@testing-library/svelte';
+import html from 'svelte-htm';
+
+describe('<Container />', () => {
+	it('renders a class for the respective container size', () => {
+		const size = faker.helpers.arrayElement(['xs', 'sm', 'md', 'lg', 'xl']);
+		const children = faker.word.noun();
+		const { getByText } = render(html`<${Container} size=${size}>${children}</${Container}>`);
+
+		expect(getByText(children)).toHaveClass(`container--${size}`);
+	});
+});
