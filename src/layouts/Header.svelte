689164f148d060da4cbe2c1909e085a382878dd2
Henrique Ramos <henriquealberone@outlook.com>
2023-06-23 16:48:37 -0300

    feat: style Header

--- src/layouts/Header.svelte
+++ src/layouts/Header.svelte
@@ -5,9 +5,29 @@
 </script>
 
 <nav class="header">
-	<Logo />
-	<Link href="/about">{$_('header.about')}</Link>
-	<Link href="/referral">{$_('header.referral')}</Link>
-	<Link href="/contact">{$_('header.contact')}</Link>
-	<Link href="/support">{$_('header.support')}</Link>
+	<Link href="/"><Logo /></Link>
+	<div class="header__links">
+		<Link href="/about">{$_('header.about')}</Link>
+		<Link href="/referral">{$_('header.referral')}</Link>
+		<Link href="/contact">{$_('header.contact')}</Link>
+		<Link href="/support">{$_('header.support')}</Link>
+	</div>
 </nav>
+
+<style lang="scss">
+	@use '$theme/utils';
+
+	.header {
+		display: flex;
+		margin: utils.spacing(4) 0;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: utils.spacing(2);
+
+		&__links {
+			gap: utils.spacing(2);
+			display: flex;
+		}
+	}
+</style>
