5efc91cfbf99debde30051cdf48c36231d1066d0
Henrique Ramos <henriquealberone@outlook.com>
2023-06-27 21:10:15 -0300

    chore: create FormInput component

---
+++ src/components/FormInput.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import type { Writable } from 'svelte/store';
+	import type { Field } from 'svelte-forms/types';
+
+	export let field: Writable<Field<string>>;
+	export let errorMessages: Record<string, string> = {};
+	export let textArea = false;
+</script>
+
+<div class="input">
+	{#if textArea}
+		<textarea class="input__field" {...$$props} bind:value={$field.value} />
+	{:else}
+		<input class="input__field" {...$$props} bind:value={$field.value} />
+	{/if}
+
+	<div class="input__error">
+		{#if $field.errors.length > 0}
+			<small>{errorMessages[$field.errors[0]]}</small>
+		{/if}
+	</div>
+</div>
+
+<style lang="scss">
+	@use '$theme/utils';
+
+	.input {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		&__field {
+			padding: utils.spacing(1.5);
+			border: none;
+			border-radius: 4px;
+			background-color: #f9f9f9;
+			color: var(--text-high);
+		}
+
+		&__error {
+			margin-left: utils.spacing(1);
+		}
+
+		textarea.input__field {
+			resize: none;
+			height: 100%;
+		}
+	}
+</style>
---
+++ tests/components/FormInput.test.ts
@@ -0,0 +1,26 @@
+import { field } from 'svelte-forms';
+
+import { faker } from '@faker-js/faker';
+import { render } from '@testing-library/svelte';
+
+import FormInput from '$components/FormInput.svelte';
+
+describe('<FormInput />', () => {
+	it('renders a HTML input element', () => {
+		const placeholder = faker.word.noun();
+		const { getByPlaceholderText } = render(FormInput, { placeholder, field: field('name', '') });
+
+		expect(getByPlaceholderText(placeholder)).toBeInstanceOf(HTMLInputElement);
+	});
+
+	it('renders a textarea if textArea === true', () => {
+		const placeholder = faker.word.noun();
+		const { getByPlaceholderText } = render(FormInput, {
+			placeholder,
+			field: field('name', ''),
+			textArea: true
+		});
+
+		expect(getByPlaceholderText(placeholder)).toBeInstanceOf(HTMLTextAreaElement);
+	});
+});
