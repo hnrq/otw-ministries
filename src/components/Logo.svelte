6411eb0e421fc17827c7280491ae222129792db8
Henrique Ramos <henriquealberone@outlook.com>
2023-06-27 22:35:10 -0300

    chore: change Logo colors

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -6,7 +6,13 @@
 <svg role="img" {width} {height} viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
 	<path
 		d="M0.369141 18.156H18.4738V0H27.5261V18.156H45.6308V27.234H27.5261V63.5461H18.4738V27.234H0.369141V18.156Z"
-		fill="black"
-		fill-opacity="0.87"
 	/>
 </svg>
+
+<style lang="scss">
+	svg {
+		path {
+			fill: var(--text-high);
+		}
+	}
+</style>
