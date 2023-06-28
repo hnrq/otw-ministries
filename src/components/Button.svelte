db07da1119d5a71fa20b912edf0c3707135a418b
Henrique Ramos <henriquealberone@outlook.com>
2023-06-27 22:35:26 -0300

    chore: change Button color

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
 	interface $$Props extends Partial<HTMLButtonElement> {
-		fullWidth: boolean;
+		fullWidth?: boolean;
 	}
 
 	export let fullWidth = false;
 </script>
 
-<button class:button--full-width={fullWidth} class="button" {...$$props} />
+<button class:button--full-width={fullWidth} class="button" {...$$props}><slot /></button>
 
 <style lang="scss">
+	@use '$theme/utils';
+
 	.button {
 		border: none;
 		outline: none;
-		background-color: var(--primary-color);
+		background-color: var(--text-high);
 		color: white;
+		padding: utils.spacing(1) utils.spacing(2);
+		border-radius: 4px;
+
+		&:hover {
+			cursor: pointer;
+		}
 
 		&--full-width {
 			width: 100%;
