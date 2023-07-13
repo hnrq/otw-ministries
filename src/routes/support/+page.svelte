e1ab73b4cb7869c836e8297ae271339aeaa55a08
Henrique Ramos <henriquealberone@outlook.com>
2023-07-13 20:55:11 -0300

    feat: create /support page

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,31 @@
+<section class="support">
+	<h2>Support us</h2>
+	<small class="support__subtitle">
+		Please help us move forward to continue to serve those in their journey to freedom and recovery.
+	</small>
+
+	<h3>PayPal</h3>
+	<p>PayPal donations can be done through... and then sent to 773-454-1545</p>
+
+	<h3>Checks</h3>
+	<p>
+		Checks can be mailed to:<br />
+		Outside the Walls Ministries<br />
+		601 S. California<br />
+		Chicago, IL. 60612<br />
+	</p>
+
+	<p>For any questions, please reach out through Contact us.</p>
+</section>
+
+<style lang="scss">
+	@use '$theme/utils';
+
+	.support {
+		&__subtitle {
+			color: var(--text-medium);
+			margin-bottom: utils.spacing(4);
+			display: block;
+		}
+	}
+</style>
