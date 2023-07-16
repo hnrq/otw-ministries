ced7819a5149f7f4cee47209e413390300723494
Henrique Ramos <henriquealberone@outlook.com>
2023-07-16 20:48:36 -0300

    feat: create Referral component

---
+++ tests/components/Referral.test.ts
@@ -0,0 +1,17 @@
+import Referral from '$components/Referral.svelte';
+import type { Referral as ReferralType } from '$components/Referral.svelte';
+import { faker } from '@faker-js/faker';
+import { render } from '@testing-library/svelte';
+
+describe('<Referral />', () => {
+	it.each(['name', 'note', 'url'])('renders the %s', (property) => {
+		const referral = {
+			name: faker.word.noun(),
+			url: faker.internet.url(),
+			note: faker.lorem.sentence()
+		};
+		const { getByText } = render(Referral, { referral });
+
+		expect(getByText(referral[property as keyof ReferralType])).toBeInTheDocument();
+	});
+});
