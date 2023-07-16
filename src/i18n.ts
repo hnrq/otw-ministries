ac54a3a18ac4ee499d3971fe4de87bf68b0d5481
Henrique Ramos <henriquealberone@outlook.com>
2023-07-16 20:48:02 -0300

    chore: stub SvelteKit stores in Vitest

--- src/i18n.ts
+++
@@ -1,8 +0,0 @@
-import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
-
-register('en', () => import('./lang/en.json'));
-
-init({
-	fallbackLocale: 'en',
-	initialLocale: getLocaleFromNavigator()
-});
