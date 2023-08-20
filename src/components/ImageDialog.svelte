b4036408613c9315a6f7ab1dab3544d2cbef80eb
Henrique Ramos <henriquealberone@outlook.com>
2023-08-20 16:42:00 -0300

    chore: add svelte-modals dependency

---
+++ src/components/ImageDialog.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+
+	export let isOpen = false;
+	export let src = '';
+	$: console.log(isOpen);
+</script>
+
+{#if isOpen}
+	<!-- on:introstart and on:outroend are required to transition 1 at a time between modals -->
+	<div role="dialog" class="modal" transition:fly={{ y: 50 }} on:introstart on:outroend>
+		<img {src} alt="" />
+	</div>
+{/if}
+
+<style>
+	.modal {
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		left: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		pointer-events: none;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,24 @@
 <script>
+	import { fade } from 'svelte/transition';
+
 	import Container from '$components/Container.svelte';
 	import Header from '$layouts/Header.svelte';
+	import { Modals, closeModal } from 'svelte-modals';
+
 	import '$theme/index.scss';
 </script>
+
+<Modals>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div
+		transition:fade
+		slot="backdrop"
+		class="backdrop"
+		on:click={closeModal}
+		role="button"
+		tabindex="0"
+	/>
+</Modals>
 
 <Header />
 <Container size="md">
@@ -10,4 +26,12 @@
 </Container>
 
 <style>
+	.backdrop {
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		left: 0;
+		background: rgba(0, 0, 0, 0.5);
+	}
 </style>
